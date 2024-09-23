faffad151776db99454d4d34ba06773884fae345
Cristian Fierro <ccamilofierro@gmail.com>
2024-09-22 19:25:27 -0500

    update design

--- script.js
+++ script.js
@@ -62,3 +62,56 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+function initializeMenu() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('.encabezado__navegacion');
+    const header = document.querySelector('.encabezado__container');
+
+    menuToggle.addEventListener('click', function(event) {
+        event.stopPropagation();
+        toggleMenu();
+    });
+
+    // Cerrar el menú al hacer clic en un enlace
+    const navLinks = document.querySelectorAll('.encabezado__navegacion--elemento a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            closeMenu();
+        });
+    });
+
+    // Cerrar el menú al hacer clic fuera de él
+    document.addEventListener('click', function(event) {
+        const isClickInsideNav = nav.contains(event.target);
+        const isClickOnMenuToggle = menuToggle.contains(event.target);
+
+        if (!isClickInsideNav && !isClickOnMenuToggle && nav.classList.contains('active')) {
+            closeMenu();
+        }
+    });
+
+    function toggleMenu() {
+        nav.classList.toggle('active');
+        menuToggle.classList.toggle('active');
+        
+        const isExpanded = menuToggle.getAttribute('aria-expanded') === 'true';
+        menuToggle.setAttribute('aria-expanded', !isExpanded);
+        
+        if (nav.classList.contains('active')) {
+            menuToggle.setAttribute('aria-label', 'Cerrar menú de navegación');
+        } else {
+            menuToggle.setAttribute('aria-label', 'Abrir menú de navegación');
+        }
+    }
+
+    function closeMenu() {
+        nav.classList.remove('active');
+        menuToggle.classList.remove('active');
+        menuToggle.setAttribute('aria-expanded', 'false');
+        menuToggle.setAttribute('aria-label', 'Abrir menú de navegación');
+    }
+}
+
+// Llamar a la función cuando se carga el DOM
+document.addEventListener('DOMContentLoaded', initializeMenu);
